c3b5e6c472a17f70d6267453c6b0bc7d8f185a76
ulisandro <sanchezulises952@gmail.com>
2022-05-25 20:31:37 -0300

    terminando slider

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-const enlaces  = document.querySelectorAll('.ul .a')
-const lightbox = document.querySelector('.lightbox')
-const grande   = document.querySelector('.grande')
-const cerrar   = document.querySelector('.cerrar')
+const enlaces  = document.querySelectorAll('.ul .a');
+const lightbox = document.querySelector('.lightbox');
+const grande   = document.querySelector('.grande');
+const cerrar   = document.querySelector('.cerrar');
 
 enlaces.forEach(( cadaEnlace , i )=>{
     enlaces[i].addEventListener('click', ( e )=>{
@@ -12,9 +12,48 @@ enlaces.forEach(( cadaEnlace , i )=>{
         lightbox.classList.add('activo')
         grande.setAttribute('src', ruta)
 
-    })
-})
+    });
+});
 
 cerrar.addEventListener('click', ()=>{
-    lightbox.classList.remove('activo')
-})
+    lightbox.classList.remove('activo');
+});
+
+
+
+// slider 
+
+// let indice = 1;
+
+// muestraSlides(indice);
+
+// function avanzaSlide(n){
+//     muestraSlides(indice += n);
+
+// }
+
+// function posicionSlide(){
+//     muestraSlides(indice = n);
+// }
+
+// function muestraSlides(n){
+//     let i;
+//     let slides = document.querySelector('.slider-imagen');
+//     let barras = document.querySelector('.barra');
+
+//     if(n > slides.length){
+//         indice = 1;
+
+//     }
+//     if(n < 1){
+//         indice =slides.length();
+//     }
+//    for(i = 0; i < slides.length; i++){
+//         slides[i].style.display= 'none'
+//       }
+//       for(i = 0; i < barras.length; i++){
+//         barras[i].className = barras[i].className.replace(' active', "")
+//       }
+//       slides[indice-1].style.display = 'block';
+//       barras[inidice-1].className += ' active'
+//    }
